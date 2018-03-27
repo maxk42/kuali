72c2309b0fe2f92c1df7a30e5441248c53f1eb92
maxk42 <max.katz.work@gmail.com>
2018-03-27 16:30:38 -0700

    Did some testing.  Fixed a number of typos.  (In a production system, unit tests would be a part of the final deliverable.)
    Cleaned up syntax.
    
    Realized my logic differed from the deliverables slightly: If I'm reading the requirements correctly, an elevator on the 30th floor heading to the first floor should answer a call on the first floor before an idling elevator on the 2nd floor.  Rather than question this deliverable, I've modified the system to deliver the desired final result.
    
    Final commit.
    
    I'd just like to add that I spend a lot of time thinking about this very problem.  The elevator in my building has to be about the slowest elevator in the world and I keep thinking how with fewer than a hundred lines of code it could be made far less annoying and with a couple thousand lines of code and a database it could be made a downright pleasure.
    
    See, the elevator in my building simply sits wherever it was last ridden to.  Because it's so slow, it's always on the wrong floor and you have to wait for it.  For instance in the morning, when everyone is heading to work, everyone is heading down to the parking garage.  So the first person to leave for work in the morning presses the button and let's say the elevator happens to be at their floor already.  The doors open immediately, the person steps in, and they enjoy their nice, slow ride down to the garage.  The elevator now sits in the garage, languishing.  After a few minutes, the next person to head to work presses the button.  No matter what floor they're on, it's not going to be the garage.  So the elevator doors close excruciatingly slowly.  The elevator gathers up it's courage, and finally lurches slowly to the floors above.  It can take nearly a full minute to reach the top floor.  (There are only three stories, plus two garage levels.)  By the time it makes it to the top floor from the bottom of the garage, a queue is likely to be forming on levels two and one, forcing it to stop at every level along the way.  If it had dumbly returned to the lobby when not answering a call, it would've saved half the time of its journey.  If it were made just slightly smarter and returned to the upper floors in the morning and the garage floors in the afternoon, much anguish would be saved.  If it were just slightly smarter than that and kept a record of what times it was summoned on which floors on particular days of the week, it would be able to anticipate its next summons, making it much, much less painful to use with absolutely no upgrade to the hardware.  But that's all beyond the scope of this project.  Thanks for the fun challenge!
    
     -Max

--- challenge.js
+++ challenge.js
@@ -1,125 +1,131 @@
 "use strict;"
 const EventEmitter = require('events');
 class Elevator extends EventEmitter {
-	constructor(floors, currentFloor) {
-		this.floors = floors;
-		this.currentFloor = floor;
-		this.destination = floor;
-		this.doorsOpen = true;
-		this.trips = 0;
-		this.floorsPassed = 0;
-		this.state = 'idle';
-	}
-	
-	openDoors() {
-		event.emit('opening doors');
-	}
-	
-	closeDoors() {
-		event.emit('closing doors');
-	}
-	
-	moveTo(floor) {
-		if(this.state == 'maintenance') {
-			this.emit('invalid request', 'moveTo', 'maintenance');
-			return false;
-		}
-		if(floor < 1 || floor > this.floors) {
-			this.emit('invalid request', 'moveTo', floor);
-			return false;
-		}
-		if(floor == this.currentFloor) {
-			return false;
-		}
-		this.emit('moving to floor', floor);
-		this.destination = floor;
-		if(this.doorsOpen) {
-			this.closeDoors();
-		}
-		this.state = 'moving';
-		while(this.currentFloor != floor) {
-			if(this.currentFloor < floor) {
-				this.moveUp();
-			} else {
-				this.moveDown();
-			}
-		}
-		this.state = 'idle';
-		this.openDoors();
-		this.trips++;
-		if(this.trips >= 100) {
-			this.state = 'maintenance';
-			this.emit('entering maintenance mode');
-			setTimeout(this.closeDoors, 10000);		// i'm assuming someone can still get out by pressing the button
-		}
-		return true;
-	}
-	
-	moveDown() {
-		if(this.currentFloor <= 1) {
-			event.emit('invalid request', 'moveDown');
-			return;
-		}
-		if(this.doorsOpen) {
-			this.closeDoors();
-		}
-		this.currentFloor--;
-		this.arriveAtFloor(this.currentFloor);
-	}
-	
-	moveUp() {
-		if(this.currentFloor >= this.floors) {
-			event.emit('invalid request', 'moveUp');
-			return;
-		}
-		if(this.doorsOpen) {
-			this.closeDoors();
-		}
-		this.currentFloor++;
-		this.arriveAtFloor(this.currentFloor);
-	}
-	
-	arriveAtFloor(floor) {
-		this.emit('arrived', floor);
-		this.floorsPassed++;
-	}
+  constructor(floors, currentFloor) {
+    super();
+    this.floors = floors;
+    this.currentFloor = currentFloor;
+    this.destination = currentFloor;
+    this.doorsOpen = true;
+    this.trips = 0;
+    this.floorsPassed = 0;
+    this.state = 'idle';
+  }
+  
+  openDoors() {
+    this.emit('opening doors');
+  }
+  
+  closeDoors() {
+    this.emit('closing doors');
+  }
+  
+  moveTo(floor) {
+    if(this.state == 'maintenance') {
+      this.emit('invalid request', 'moveTo', 'maintenance');
+      return false;
+    }
+    if(floor < 1 || floor > this.floors) {
+      this.emit('invalid request', 'moveTo', floor);
+      return false;
+    }
+    if(floor == this.currentFloor) {
+      return false;
+    }
+    this.emit('moving to floor', floor);
+    this.destination = floor;
+    if(this.doorsOpen) {
+      this.closeDoors();
+    }
+    this.state = 'moving';
+    while(this.currentFloor != floor) {
+      if(this.currentFloor < floor) {
+        this.moveUp();
+      } else {
+        this.moveDown();
+      }
+    }
+    this.state = 'idle';
+    this.openDoors();
+    this.trips++;
+    if(this.trips >= 100) {
+      this.state = 'maintenance';
+      this.emit('entering maintenance mode');
+      setTimeout(this.closeDoors, 10000);    // i'm assuming someone can still get out by pressing the button
+    }
+    return true;
+  }
+  
+  moveDown() {
+    if(this.currentFloor <= 1) {
+      this.emit('invalid request', 'moveDown');
+      return;
+    }
+    if(this.doorsOpen) {
+      this.closeDoors();
+    }
+    this.currentFloor--;
+    this.arriveAtFloor(this.currentFloor);
+  }
+  
+  moveUp() {
+    if(this.currentFloor >= this.floors) {
+      this.emit('invalid request', 'moveUp');
+      return;
+    }
+    if(this.doorsOpen) {
+      this.closeDoors();
+    }
+    this.currentFloor++;
+    this.arriveAtFloor(this.currentFloor);
+  }
+  
+  arriveAtFloor(floor) {
+    this.emit('arrived', floor);
+    this.floorsPassed++;
+  }
 }
 
 class ElevatorController {
-	constructor(nElevators, floors) {
-		this.elevators = [];
-		for(let i = 0; i < nElevators; i++) {
-			this.elevators.push(new Elevator(floors, 1));
-		}
-	}
-	
-	requestElevator(fromFloor, direction) {
-		let targetElevator = false;
-		while(targetElevator === false) {	// repeat until an elevator becomes available
-			for(let elevatorIndex in this.elevators) {
-				let elevator = elevators[elevatorIndex];
-				if(elevator.currentFloor == fromFloor) {
-					elevatorFound = elevatorIndex;
-					break;
-				}
-				if(elevator.state == "maintenance") {
-					continue;
-				}
-				// Precompute a few conditionals to make the following `if` statement readable
-				let truthTable = {
-					moving: elevator.state == "moving",
-					movingPast: (elevator.floor >= fromFloor && elevator.destination <= fromFloor)
-						|| (elevator.floor <= fromFloor && elevator.destination >= fromFloor),
-					elevatorIsCloser: Math.abs(elevator.floor - fromFloor) < Math.abs(elevators[targetElevator].floor - fromFloor)
-				};
-				if( (truthTable.movingPast || !truthTable.moving) && (truthTable.elevatorIscloser || targetElevator === false) ) {
-					targetElevator = elevatorIndex;
-				}
-			}
-		}
-	}
-	
+  constructor(nElevators, floors) {
+    this.elevators = [];
+    for(let i = 0; i < nElevators; i++) {
+      this.elevators.push(new Elevator(floors, 1));
+    }
+  }
+  
+  requestElevator(fromFloor) {
+    let targetElevator = false;
+    let nearestIsMoving = false;
+    while(targetElevator === false) {  // repeat until an elevator becomes available
+      for(let elevatorIndex in this.elevators) {
+        let elevator = this.elevators[elevatorIndex];
+        if(elevator.currentFloor == fromFloor) {
+          elevatorFound = elevatorIndex;
+          break;
+        }
+        if(elevator.state == "maintenance") {
+          continue;
+        }
+        // Precompute a few conditionals to make the following `if` statement readable
+        let truthTable = {
+          moving: elevator.state == "moving",
+          movingPast: (elevator.floor >= fromFloor && elevator.destination <= fromFloor)
+            || (elevator.floor <= fromFloor && elevator.destination >= fromFloor),
+          elevatorIsCloser: targetElevator === false ? false : Math.abs(elevator.floor - fromFloor) < Math.abs(this.elevators[targetElevator].floor - fromFloor)
+        };
+        if( (truthTable.movingPast || !truthTable.moving) && (truthTable.elevatorIscloser || targetElevator === false) ) {
+          if(targetElevator !== false && nearestIsMoving && !truthTable.moving && elevator.floor != fromFloor) {	// skip idling elevators if we've already found one headed our way, unless it's on the same level
+            continue;
+	  }
+          targetElevator = elevatorIndex;
+          if(truthTable.moving) {
+            nearestIsMoving = true;
+	  }
+        }
+      }
+    }
+  }
 }
 
 
-x = new ElevatorController(10, 100)
